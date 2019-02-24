9fecc67c9f4db97c2fbfbbedd3f80f6c3c6fa921
Nobske <35899690+Nobske@users.noreply.github.com>
2019-02-24 14:24:36 +0100

    Add files via upload

---
+++ Circle-Calculator.py
@@ -0,0 +1,18 @@
+
+# coding: utf-8
+
+# In[ ]:
+
+
+import numpy as np
+pi = np.pi
+radius = int(input("Give me the Radius of a circle please"))
+def diameter(x):
+    return 2*radius
+def area(x):
+    return pi*radius*radius
+def circumference(x):
+    return pi* diameter(x)
+print("You put in the radius of {}, the diameter is {}, the area is {} and the circumference is {}".format(radius, diameter(radius), area(radius), circumference(diameter(radius))))
+close = input("Press any button to close")
+
